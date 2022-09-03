96c60c650e9b5ee20699d259bc4053f1ecb7bffa
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-09-03 17:37:52 +0530

    Assignment 1

---
+++ Assignment-1/problem2.js
@@ -0,0 +1,7 @@
+let one, two, three, four, five;
+one=1;
+two=2;
+three=3;
+four=4;
+five=5;
+console.log(2*(one+two+three+four+five));
---
+++ Assignment-1/problem3.js
@@ -0,0 +1,15 @@
+let one, two, three, four, five, six;
+one = 1;
+two =2;
+three = 3;
+four = 4;
+five = 5; 
+six = 6;
+
+two=2*two;
+five=2*five;
+six=2*six;
+three=3*three;
+four=3*four;
+
+console.log(one+two+three+four+five+six);
