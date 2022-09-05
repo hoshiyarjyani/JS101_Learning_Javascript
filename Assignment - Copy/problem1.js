6f1c490f512fd2391d19fd582307ac15b6314432
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-09-05 23:36:22 +0530

    Assignment 4 riya

---
+++ Assignment - Copy/problem1.js
@@ -0,0 +1,4 @@
+//Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai School");
+console.log("A Transformation in Education");
---
+++ Assignment - Copy/problem3.js
@@ -0,0 +1,9 @@
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+var name = "Hoshiyar Singh";
+var age =23;
+console.log(name + " " + age);
+console.log(typeof(name) +" "+ typeof(age));
