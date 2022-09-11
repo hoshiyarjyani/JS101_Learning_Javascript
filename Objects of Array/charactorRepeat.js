cc5e5e4c382cee18ea75900d403296b691838d1a
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-09-11 23:57:03 +0530

    update

--- Objects of Array/charactorRepeat.js
+++ Objects of Array/charactorRepeat.js
@@ -1,10 +1,9 @@
 let x = "Hoshiyarsingh";
 let obj = {};
-for (let i = 0; i < x.length; i++) {
+for (let i = 0; i <x.length; i++) {
   if (obj[x[i]] == undefined) {
     obj[x[i]] = 1;
   } else {
     obj[x[i]]++;
-  }
 }
 console.log(obj);
