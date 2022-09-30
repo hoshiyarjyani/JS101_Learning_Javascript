f44395425fc7cdf7565379eab170a724f3735922
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-09-30 09:21:24 +0530

    singleInteger SingleLine BoilerPlateCode

---
+++ Input Taking BoilerPlateCode/SingleLineSingleInteger.js
@@ -0,0 +1,33 @@
+function addone(input) {
+  let add = input + 1;
+  console.log(add);
+}
+
+function runProgram(input) {
+  // Write Code Here
+
+  input = +input;
+  addone(input);
+}
+
+if (process.env.USERNAME === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
+runProgram(4);
